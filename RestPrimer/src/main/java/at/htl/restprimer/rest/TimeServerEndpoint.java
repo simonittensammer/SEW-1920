cb245c8c9a3b489151c9b964fab72bca9908f934
Simon <simon.ittensammer@gmx.at>
2019-10-04 14:51:20 +0200

    initalized RestPrimer

---
+++ RestPrimer/src/main/java/at/htl/restprimer/rest/TimeServerEndpoint.java
@@ -0,0 +1,14 @@
+package at.htl.restprimer.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.time.LocalDateTime;
+
+@Path("time")
+public class TimeServerEndpoint {
+
+    @GET
+    public String time() {
+        return "Time" + LocalDateTime.now();
+    }
+}
